47e2aa139f35063bd8c2ca26f08f014ef7bc1582
Paulo <innorammara@gmail.com>
2021-01-18 16:29:56 +0100

    improved efficiency and verbose

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import json
 import queue
 import time
+import winsound
 from threading import Thread
 
-import pandas as pd
+import pandas
 import requests
 
 from trade import Operation
@@ -20,25 +21,34 @@ taker_fee_krk = .0013
 taker_fee_trt = .0020
 save_interval = 20
 rate = 10
+eff_threshold = 0.2
 
 _params_krk = {"pair": "BTCEUR", "count": "4"}
 _params_trt = {}
 krk_que = queue.Queue()
 trt_que = queue.Queue()
-krk_que_trade = queue.Queue()
-trt_que_trade = queue.Queue()
-krk_que_balance = queue.Queue()
-trt_que_balance = queue.Queue()
+all_balance = dict()
 
 
 def main():
     op = Operation(trt_apikey, trt_secret, krk_apikey, krk_secret)
+    checkbalance = True
     while 1:
+        if checkbalance:
+            all_balance = op.doop(0, "krk", 0, 0, 0, 0, "trt", 0, 0, 0, 2)
+            print(all_balance)
+            try:
+                dis1 = all_balance[0]["krkbtc"] / all_balance[0]["trtbtc"]
+                dis2 = all_balance[0]["krkbch"] / all_balance[0]["trteur"]
+            except:
+                dis1 = 1
+                dis2 = 1
+            checkbalance = False
         last_bid = 0
         last_ask = 0
         depth = 0
-        _start_time = time.time()
         time.sleep(1 / rate)
+        _start_time = time.time()
         _query_time = time.time()
 
         krk_thread = Thread(target=lambda q, arg1, arg2: q.put(query(arg1, arg2)), args=(krk_que, "krk", _params_krk))
@@ -58,13 +68,17 @@ def main():
         asks_trt = loaded_json_trt['asks'][0]['price']
         bids_trt = loaded_json_trt['bids'][0]['price']
 
-        print("[i]", asks_krk)
-        print("[i]", bids_trt)
+        print("[i] ASK KRK:", asks_krk)
+        print("[i] BID TRT:", bids_trt)
+        print("[i]                           DIFFERENCE:", round(bids_trt - asks_krk, 2))
+        print("[i]                     DIFFERENCE + FEE:", round((bids_trt * (1 - taker_fee_trt)) - (asks_krk * (1 + taker_fee_krk)), 2))
+        print("[i] ASK TRT:", asks_trt)
+        print("[i] BID KRK:", bids_krk)
+        print("[i]                           DIFFERENCE:", round(bids_krk - asks_trt,2))
+        print("[i]                     DIFFERENCE + FEE:",
+              round((bids_krk * (1 - taker_fee_krk)) - (asks_trt * (1 + taker_fee_trt)), 2))
 
-        print("[i]", asks_trt)
-        print("[i]", bids_krk)
-        # Trade.dotrade("trt", "BTCEUR", "buy", 1, 1, apikey, secret)
-        if (bids_trt * (1 + taker_fee_trt)) - (asks_krk * (1 + taker_fee_krk)) > 0:
+        if (bids_trt * (1 - taker_fee_trt)) - (asks_krk * (1 + taker_fee_krk)) > 0:
             print("[!] %f < %f BUY KRK | SELL TRT DIFF: %f (MENO FEE): %f" % (
                 asks_krk, bids_trt, bids_trt - asks_krk,
                 (bids_trt * (1 + taker_fee_trt)) - (asks_krk * (1 + taker_fee_krk))))
@@ -77,13 +91,14 @@ def main():
             print("[i] NEED %f EUR | %f BTC" % (asks_krk * depth, depth))
             last_ask = asks_krk
             last_bid = bids_trt
-            if  eff > 0.1 and prod * 100 > 0.1 :
+            if prod * 100 > eff_threshold:
+                checkbalance = True
                 print("[#] TRADE")
-                op.dotrade("buy", "krk", "BTCEUR", depth, last_ask,
-                           "sell", "trt", "BTCEUR", depth, last_bid)
-                resp_trt_trade = json.loads(trt_que_trade.get())
-                resp_krk_trade = json.loads(krk_que_trade.get())
-                print(resp_trt_trade, resp_krk_trade)
+                resp_dict = op.doop("buy", "krk", "BTCEUR", depth, last_ask,
+                                    "sell", "trt", "BTCEUR", depth, last_bid, 1)
+
+                if resp_dict['krk'] != "ERROR" or resp_dict['trt'] == "ERROR":
+                    time.sleep(1)
 
         elif (bids_krk * (1 + taker_fee_krk)) - (asks_trt * (1 + taker_fee_trt)) > 0:
             print("[!] %f < %f BUY TRT | SELL KRK DIFF: %f (MENO FEE): %f" % (
@@ -98,16 +113,16 @@ def main():
             print("[!] NEED %f EUR | %f BTC" % (asks_trt * depth, depth))
             last_ask = asks_trt
             last_bid = bids_krk
-            if eff > 0.1 and prod * 100 > 0.1 :
+            if prod * 100 > eff_threshold:
+                checkbalance = True
                 print("[#] TRADE")
-                op.dotrade("buy", "trt", "BTCEUR", depth, last_ask,
-                           "sell", "krk", "BTCEUR", depth, last_bid)
-                resp_trt_trade = json.loads(trt_que_trade.get())
-                resp_krk_trade = json.loads(krk_que_trade.get())
-                print(resp_trt_trade, resp_krk_trade)
+                winsound.Beep(1132, 1000)
+                resp_dict = op.doop("buy", "trt", "BTCEUR", depth, last_ask,
+                                    "sell", "krk", "BTCEUR", depth, last_bid, 1)
+                if resp_dict['krk'] != "ERROR" or resp_dict['trt'] == "ERROR":
+                    time.sleep(1)
 
         _end_time = time.time()
-        print("[i] TIME:", int(_end_time % 100))
         if last_ask != 0:
             _list.append([_end_time, time.time(), last_ask, last_bid, depth, eff, int(_query_time * 1000),
                           (int((_end_time - _start_time) * 1000) - int(_query_time * 1000)),
@@ -123,7 +138,7 @@ def main():
 
 
 def save(list):
-    df = pd.DataFrame(list)
+    df = pandas.DataFrame(list)
     try:
         df.to_csv('file.csv', index=False, sep=';', mode='a', header=False, decimal=',')
     except:
@@ -139,23 +154,6 @@ def query(exchange, params):
         if exchange == "krk":
             resp_krk = requests.get('https://api.kraken.com/0/public/Depth', params=params)
             return resp_krk.text
-
-
-def balance():
-    list = []
-    resp_balance_trt_E = Thread(target=lambda q, arg1, arg2, arg3, arg4, arg5, arg6, arg7: q.put(
-        Operation.balance(arg1, arg2, arg3, arg4)),
-                                args=(trt_que_balance, "trt", "BTC", trt_apikey, trt_secret))
-    resp_balance_trt_B = Thread(target=lambda q, arg1, arg2, arg3, arg4, arg5, arg6, arg7: q.put(
-        Operation.balance(arg1, arg2, arg3, arg4)),
-                                args=(krk_que_balance, "krk", "EUR", krk_apikey, krk_secret))
-    resp_balance_trt_B.start()
-    resp_balance_trt_E.start()
-    resp_balance_trt_B.join()
-    resp_balance_trt_E.join()
-    list.append(trt_que_balance.get())
-
-    list.append(trt_que_balance.get())
 
 
 if __name__ == "__main__":
--- trade.py
+++ trade.py
@@ -27,56 +27,89 @@ class Operation:
         self.apikey_krk = apikey_krk
         self.secret_krk = secret_krk
 
-    def trade(self,exchange, fund_id,type, amount, price):
+    def trade(self, exchange, fund_id, side, amount, price):
         nonce = str(int(time.time() * 1e6))
         if exchange == "trt":
             url = "https://api.therocktrading.com/v1/funds/" + fund_id + "/orders"
-            payload_trt = {"fund_id": fund_id, "side": type, "amount": amount, "price": price}
+            payload_trt = {"fund_id": "BTCEUR", "side": "sell", "amount": "4", "price": "150"}
             signature = hmac.new(self.secret_trt.encode(), msg=(str(nonce) + url).encode(),
                                  digestmod=hashlib.sha512).hexdigest()
 
             _headers = {'User-Agent': 'PyRock v1', "Content-Type": "application/json", "X-TRT-KEY": self.apikey_trt,
                         "X-TRT-SIGN": signature, "X-TRT-NONCE": nonce}
-            resp = str(requests.post(url, data=json.dumps(payload_trt), headers=_headers, timeout=30))
-            return resp
+            resp = requests.post(url, data=json.dumps(payload_trt), headers=_headers)
+            return resp.text
         elif exchange == "krk":
             api = krakenex.API(self.apikey_krk, self.secret_krk)
             k = KrakenAPI(api)
-            resp = k.add_standard_order(fund_id, type, "limit", str(amount), str(price))
+            resp = k.add_standard_order(fund_id, side, "limit", str(amount), str(price))
             resp = str(resp).replace("\'", "\"")
             return resp
 
-    def dotrade(self, type, exchange, fund_id, amount, price, type2, exchange2, fund_id2, amount2, price2):
-        d = dict()
-        trt_trade = Thread(target=lambda q, arg1, arg2, arg3, arg4,arg5: q.put(
-            self.trade(arg1, arg2, arg3, arg4,arg5)),
-                           args=(q1, exchange, fund_id, type, amount, price))
-        krk_trade = Thread(target=lambda q, arg1, arg2, arg3, arg4,arg5: q.put(
-            self.trade(arg1, arg2, arg3, arg4,arg5)),
-                           args=(q2, exchange2, fund_id2, type2, amount2, price2))
-        trt_trade.start()
-        krk_trade.start()
-        trt_trade.join()
-        krk_trade.join()
-        d['trt'] = json.loads(q1.get())['order']
-        d['krk'] = json.loads(q2.get())['txid']
-        return d
-
-    def balance(self,exchange, fund, apikey, secret):
+    def balance(self, exchange):
         nonce = str(int(time.time() * 1e6))
+        d = dict()
         if exchange == "trt":
 
-            url = "https://api.therocktrading.com/v1/balances/" + fund
-            signature = hmac.new(secret.encode(), msg=(str(nonce) + url).encode(),
+            url = "https://api.therocktrading.com/v1/balances"
+            signature = hmac.new(self.secret_trt.encode(), msg=(str(nonce) + url).encode(),
                                  digestmod=hashlib.sha512).hexdigest()
-            _headers = {'User-Agent': 'PyRock v1', "Content-Type": "application/json", "X-TRT-KEY": apikey,
+            _headers = {"Content-Type": "application/json", "X-TRT-KEY": self.apikey_trt,
                         "X-TRT-SIGN": signature, "X-TRT-NONCE": nonce}
-            resp = requests.post(url, headers=_headers, timeout=30)
-            answ = json.loads(str(resp))['balance']
-            return answ
+            resp = requests.get(url, headers=_headers)
+            d["trtbtc"] = json.loads(resp.text)["balances"][0]["balance"]
+            d["trteur"] = json.loads(resp.text)["balances"][8]["balance"]
+            return d
         elif exchange == "krk":
-            api = krakenex.API(apikey, secret)
+            api = krakenex.API(self.apikey_krk, self.secret_krk)
             k = KrakenAPI(api)
-            resp = k.get_trade_balance(asset=fund)
-            resp = str(resp).replace("\'", "\"")
-            return resp
+            resp = k.get_account_balance()
+            d["krkbtc"] = resp["vol"]["XXBT"]
+            d["krkbch"] = resp["vol"]["BCH"]
+            return d
+
+    def doop(self, side, exchange, fund_id, amount, price, side2, exchange2, fund_id2, amount2, price2, case):
+        if case == 1:
+
+            d = dict()
+            krk_trade = Thread(target=lambda q, arg1, arg2, arg3, arg4, arg5: q.put(
+                self.trade(arg1, arg2, arg3, arg4, arg5)),
+                               args=(q1, exchange, fund_id, side, amount, price))
+            trt_trade = Thread(target=lambda q, arg1, arg2, arg3, arg4, arg5: q.put(
+                self.trade(arg1, arg2, arg3, arg4, arg5)),
+                               args=(q2, exchange2, fund_id2, side2, amount2, price2))
+            trt_trade.start()
+            krk_trade.start()
+            trt_trade.join()
+            krk_trade.join()
+            try:
+                d["krk"] = json.loads(q1.get())["txid"]
+            except:
+                d["krk"] = "ERROR"
+            try:
+                d["trt"] = json.loads(q2.get())["order"]
+            except:
+                d["trt"] = "ERROR"
+
+            return d
+        if case == 2:
+            d = dict()
+            krk_balance = Thread(target=lambda q, arg1: q.put(
+                self.balance(arg1)),
+                                 args=(q1, exchange))
+            trt_balance = Thread(target=lambda q, arg1: q.put(
+                self.balance(arg1)),
+                                 args=(q2, exchange2))
+            trt_balance.start()
+            krk_balance.start()
+            trt_balance.join()
+            krk_balance.join()
+
+            d = (q1.get(), q2.get())
+
+            return d
+
+    def merge_two_dicts(x, y):
+        z = x.copy()
+        z.update(y)
+        return z
