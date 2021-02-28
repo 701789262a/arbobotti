79a8e436c604b5b8ac7cf265161552da3d8feb0c
Paulo <innorammara@gmail.com>
2021-02-28 05:31:58 +0100

    improved log

--- logic.py
+++ logic.py
@@ -428,7 +428,7 @@ def telegram(_list):
         message = ("EXECUTED TRADE AT " + str(_tg_list[0][0]) + ": BOUGHT " + str(_list[0][3]) + " BTC @" + str(
             _list[0][4]) + " ON " + str(
             _list[0][2]) + " SOLD @" + str(_list[0][7]) + " ON " + str(_tg_list[0][6]) + ". CALCULATED GAIN = " + str(
-            _tg_list[1][13] - _tg_list[0][13])).replace(" ", "%20")
+            _tg_list[1][12] - _tg_list[0][12])).replace(" ", "%20")
         bot_token = "1673298427:AAHsEtcRBMzkaWbtbSQexRhgJtOiHzJuXqw"
         bot_chatID = "-1001175272795"
         print(str(bot_token))
