2a6c787f7078cd7b1afd94e7f30b615cc455c46a
Paulo <innorammara@gmail.com>
2021-03-04 03:10:57 +0100

    bug fix

--- logic.py
+++ logic.py
@@ -458,7 +458,7 @@ def db(_list):
 
 def telegram(_list):
     message = ("EXECUTED TRADE AT " + str(_list[0][0]) + ":\nBOUGHT <b>" + str(
-        round(float(_list[0][3]), 8)) + "</b> $BTC <b>" + str(
+        round(float(_list[0][3].replace(",",".")), 8)) + "</b> $BTC <b>" + str(
         _list[0][4]) + "</b> ON <code>" + str(
         _list[0][2]) + "</code> SOLD <b>" + str(_list[0][7]) + "</b> ON <code>" + str(
         _list[0][6]) + "</code>. CALCULATED GAIN = <b>" + str(
