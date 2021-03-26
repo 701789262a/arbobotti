ea13d86a4c7cb850c080bf594c0b06f98ce1c898
Paulo <innorammara@gmail.com>
2021-03-26 01:56:24 +0100

    added auto updater

--- updater.py
+++ updater.py
@@ -6,10 +6,16 @@ import time
 def updater(q):
     time.sleep(20)
     while True:
-        proc = subprocess.Popen('git remote show origin', shell=True, stdout=subprocess.PIPE,)
-        output = proc.communicate()[0]
-        if "out of date" in output.decode("utf-8"):
-            os.system("git pull origin master")
-            time.sleep(2)
-            q.put(1)
-        time.sleep(10)
+        try:
+            proc = subprocess.Popen('git remote show origin', shell=True, stdout=subprocess.PIPE,)
+            output = proc.communicate()[0]
+            if "out of date" in output.decode("utf-8"):
+                os.system("git pull origin master")
+                time.sleep(2)
+                q.put(1)
+            time.sleep(10)
+        except Exception as err:
+            print(err)
+            with open("err.txt","w") as f:
+                f.write(err)
+                f.close()
