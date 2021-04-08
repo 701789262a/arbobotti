2623082c2ba5b48ef26f1b3acbce1a0bf114b2df
Paulo <innorammara@gmail.com>
2021-04-08 02:38:15 +0200

    minor fix

--- logic.py
+++ logic.py
@@ -558,7 +558,6 @@ def db(_list, db_data):
 
 def telegram(_list, tg_data):
     log(log_type="TRADELIST", message=str(_list))
-
     message = ("EXECUTED TRADE AT " + str(_list[0][0]) + ":\nBOUGHT <b>" + str(
         round(float(_list[0][3].replace(",", ".")), 8)) + "</b> $BTC <b>" + str(
         _list[0][4]) + "</b> ON <code>" + str(
@@ -567,7 +566,7 @@ def telegram(_list, tg_data):
         round(float(_list[1][11]) + float(_list[1][10]) - float(
             _list[0][11]) - float(_list[0][10]),
               5)) + "€</b>" + "\nSPREAD: <b>" + str(
-        round(float(_list[0][7]) - float(_list[0][4]))) + "€</b>").replace(" ",
+        round(float(_list[0][7].replace(",",".")) - float(_list[0][4]).replace(",","."))) + "€</b>").replace(" ",
                                                                                                                "%20")
     bot_token = tg_data["token"]
     bot_chatID = tg_data["app_id"]
--- predict.py
+++ predict.py
@@ -15,12 +15,12 @@ end_date = '03/04/2021'
 data = pd.read_csv(r'filev2server.csv', index_col="DATE", parse_dates=True, delimiter=",")
 data.index = pd.to_datetime(data.index)
 data = data.stack().str.replace(',', '.').unstack()
-data_train = data[(data.index >= '2021-03-25') & (data.index < '2021-03-29')]
-print(data_train['TRT-BNB'].astype(str).astype(float))
-data_train['TRT-BNB'].astype(str).astype(float).plot()
+data_train = data[(data.index >= '2021-03-25') & (data.index < '2021-03-29')]["TRT-BNB"]
+print("primo",data_train['TRT-BNB'].astype(str).astype(float))
+data_train.astype(str).astype(float).plot()
 plt.show()
 scaler = MinMaxScaler(feature_range=(0, 1))
-data_train = np.array(data).reshape(-1, 1)
+data_train = np.array(data_train).reshape(-1, 1)
 scaled_data = scaler.fit_transform(data_train)
 
 x_train = []
@@ -32,51 +32,56 @@ for x in range(prediction_days, len(scaled_data)):
 
 x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-print(y_train)
+print("ytrain",y_train)
 model = Sequential()
 
 model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50,return_sequences=True))
 model.add(Dropout(0.2))
 model.add(LSTM(units=25))
 model.add(Dropout(0.2))
 model.add(Dense(units=1))
 
 model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(x_train, y_train, epochs=2, batch_size=1280)
+model.fit(x_train, y_train, epochs=2, batch_size=128)
 
 start_test = '04/02/2021'
 end_test = '07/02/2021'
 
 ''' Test model with existing data '''
 
-data_test = data[(data.index >= '2021-04-02') & (data.index < '2021-04-06')]
-print(data_test['TRT-BNB'].astype(str).astype(float))
-data_test['TRT-BNB'].astype(str).astype(float).plot()
+data_test = data[(data.index >= '2021-04-01') & (data.index < '2021-04-04')]["TRT-BNB"]
+print("train",data_test['TRT-BNB'].astype(str).astype(float))
+data_test.astype(str).astype(float).plot()
 plt.show()
 scaler = MinMaxScaler(feature_range=(0, 1))
-data_test = np.array(data).reshape(-1, 1)
+data_test = np.array(data_test).reshape(-1, 1)
 scaled_data = scaler.fit_transform(data_test)
-print(scaled_data)
+print("scaled",scaled_data)
 print(data_test[len(data_test) - 1])
 print(data_test[1])
 
 model_input = np.array(data_test).reshape(-1, 1)
 model_input = scaler.transform(model_input)
 x_test = []
-for x in range(prediction_days, len(model_input)):
-    x_test.append(model_input[x - prediction_days:x, 0])
+for x in range(100, len(model_input)):
+    x_test.append(model_input[x - 100:x, 0])
 
 x_test = np.array(x_test)
 x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-print(x_test)
+print("xtest",x_test.shape)
 predicted_prices = model.predict(x_test)
 predicted_prices = scaler.inverse_transform(predicted_prices)
 
-real_data = [model_input[len(model_input) + 1 - prediction_days:len(model_input + 1), 0]]
+real_data =data[(data.index >= '2021-04-05') & (data.index < '2021-04-06')]["TRT-BNB"]
 real_data = np.array(real_data)
-real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
+real_data = np.array(real_data).reshape(-1, 1)
+real_data = scaler.transform(real_data)
+trial=np.array(real_data)
+trial=np.reshape(trial, (trial.shape[0], trial.shape[1], 1))
 print ("bobotto")
-prediction = model.predict(real_data)
+prediction = model.predict(trial)
 print("due",prediction)
 
 prediction = scaler.inverse_transform(prediction)
